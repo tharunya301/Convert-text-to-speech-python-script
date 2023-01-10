24b5ab58afde325afdf9818cd14637eb2050c9a1
Tharunya Harshani <106473199+tharunya301@users.noreply.github.com>
2023-01-10 21:25:12 +0530

    Delete Text_to_speech.py

--- Text_to_speech.py
+++
@@ -1,6 +0,0 @@
-import pyttsx3
-engine = pyttsx3.init()
-engine.setProperty('rate', 150)    # Speed percent
-engine.setProperty('volume', 0.9)  # Volume 0-1
-engine.say("Hello I'm Tharunya Harshani.")
-engine.runAndWait()
